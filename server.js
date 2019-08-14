efbc71e7a48e73e9377f3867c3c79ba376320b8f
anthonyvigz <anthonyvigz@gmail.com>
2019-08-14 19:06:36 -0400

    server is up and running

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ require('dotenv').config();
 
 const port = process.env.PORT;
 
+server.use(express.json());
+
 server.listen(port, () => {
     console.log(`\n*** Server running on http://localhost:${port} ***\n`)
+})
+
+server.get('/', (req, res) => {
+    res.status(200).send("Welcome to Anthony's server!")
 })
