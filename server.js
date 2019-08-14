afdda200f9dc4ed4a63e4e4e69f6db4295a97c22
anthonyvigz <anthonyvigz@gmail.com>
2019-08-14 19:10:53 -0400

    added basic middleware to main server

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const express = require('express')
-
+const express = require('express');
 const server = express();
+const helmet = require('helmet');
+const morgan = require("morgan");
 
 require('dotenv').config();
 
 const port = process.env.PORT;
 
+/// applying middleware to server
+
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+
+
+
+
+
+/// listening for server
 
 server.listen(port, () => {
     console.log(`\n*** Server running on http://localhost:${port} ***\n`)
 })
+
+/// main server get request
 
 server.get('/', (req, res) => {
     res.status(200).send("Welcome to Anthony's server!")
